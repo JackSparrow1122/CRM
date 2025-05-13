c7961352371e8803a88a3e6502f501d588a8d607
Deep Mahire <dipratnamahire2222@gmail.com>
2025-05-13 17:19:14 +0530

    Update LoginPage.jsx

--- src/Component/LoginPage.jsx
+++ src/Component/LoginPage.jsx
@@ -17,7 +17,7 @@ const Add_User = () => {
     e.preventDefault();
     setLoading(true); 
     try {
-      const response = await axios.post('api/auth', user);
+      const response = await axios.post('contactmbackend-production.up.railway.app/auth', user);
       console.log('Response:', response.data);
      
       if(response.data=="ok"){
